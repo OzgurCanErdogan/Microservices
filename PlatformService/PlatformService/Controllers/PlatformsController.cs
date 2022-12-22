5209b6154006da675fb8acbb1ef30592339b3c62
OzgurCanErdogan <erdogan.ozgur.can@gmail.com>
2022-12-23 01:44:13 +0300

    Controller Methods Added

--- PlatformService/PlatformService/Controllers/PlatformsController.cs
+++ PlatformService/PlatformService/Controllers/PlatformsController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using PlatformService.Data;
 using PlatformService.Dtos;
+using PlatformService.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -32,6 +33,28 @@ namespace PlatformService.Controllers
             return Ok(_mapper.Map<IEnumerable<PlatformReadDto>>(platformItem));
         }
 
+        [HttpGet("{id}",Name = "GetPlatformById")]
+        public ActionResult<PlatformReadDto> GetPlatformById(int id)
+        {
+            Console.WriteLine("--> Getting Platform by id " + id);
+            var platformItem = _repository.GetPlatformById(id);
+            if (platformItem != null)
+            {
+                return Ok(_mapper.Map<PlatformReadDto>(platformItem));
+            }
+            return NotFound();
+        }
 
+        [HttpPost]
+        public ActionResult<PlatformReadDto> CreatePlatform(PlatformCreateDto platformData)
+        {
+            Console.WriteLine("--> Creating Platform... ");
+            var platformModel = _mapper.Map<Platform>(platformData);
+            _repository.CreatePlatform(platformModel);
+            _repository.SaveChanges();
+
+            var platformReadDto = _mapper.Map<PlatformReadDto>(platformModel);
+            return CreatedAtRoute(nameof(GetPlatformById), new { Id = platformReadDto.Id }, platformReadDto);
+        }
     }
 }
