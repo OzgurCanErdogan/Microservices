bc77a2244da24b9516303eb6bd88cf4f375e17c8
OzgurCanErdogan <erdogan.ozgur.can@gmail.com>
2023-01-19 19:06:42 +0300

    Kubernates Build
    Kubernates settings and yaml files are added

--- PlatformService/PlatformService/obj/Debug/net5.0/PlatformService.AssemblyInfo.cs
+++ PlatformService/PlatformService/obj/Debug/net5.0/PlatformService.AssemblyInfo.cs
@@ -11,6 +11,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("927a41da-4b47-4ed5-8519-7032e3c88572")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("PlatformService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
